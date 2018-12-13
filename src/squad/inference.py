55fa81c96aac165457fff7c4d43ae6f588e70ef7
Dat Nguyen <nptdat@gmail.com>
2018-12-14 00:12:45 +0900

    Fix model filepath for demo

--- src/squad/inference.py
+++ src/squad/inference.py
@@ -19,8 +19,7 @@ def load_model():
     data_binary = SquadData.load(np_path=cf.SQUAD_NP_DATA)
     word_vectors, char_vectors, train_ques_ids, X_train, y_train, val_ques_ids, X_valid, y_valid = data_binary
     model = QANetModel(cf, data_binary, data)
-    filename = 'model/qanet_ep8.h5'
-    # filename = 'model/qanet_No1/qanet_No1_ep25.h5'
+    filename = 'model/qanet_ep15.h5'
     model.load_weights(filename)
 
     # Predict once to overcome the error in API request
